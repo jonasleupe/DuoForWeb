eb87ad59bda6ede329358bc20c3266e76af9199c
Jonas Leupe <jonas.leupe@student.howest.be>
2016-12-04 16:53:40 +0100

    updated css

--- src/js/components/Video.jsx
+++ src/js/components/Video.jsx
@@ -1,12 +1,11 @@
 import React, {PropTypes} from 'react';
 
-const Video = ({meta, stream}) => {
+const Video = ({stream}) => {
 
   if (stream) stream = URL.createObjectURL(stream);
 
   return (
     <article className='video'>
-      <div className='meta'>{meta}</div>
       <video autoPlay src={stream}></video>
     </article>
   );
