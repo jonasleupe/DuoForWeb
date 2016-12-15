105860e2bd17d85babff954289c9fcb75123c9fc
Jonas Leupe <jonas.leupe@student.howest.be>
2016-12-15 23:33:47 +0100

    click icon to start speaking + speak result will be shown

--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -9,7 +9,9 @@ class App extends Component {
 
   state = {
     youStream: undefined,
-    strangerStream: undefined
+    strangerStream: undefined,
+    voiceActive: false,
+    voiceCommand: ``
   }
 
   initPeer = () => {
@@ -72,6 +74,31 @@ class App extends Component {
 
   handleYouStreamError = e => console.error(e);
 
+  handleStartVoiceCommands = e => {
+    e.preventDefault();
+    console.log(`click`);
+
+    this.setState({voiceActive: true});
+
+    annyang.setLanguage(`nl-NL`);
+    annyang.addCallback(`result`, function(userSaid) {
+      console.log(userSaid[0]);
+      this.setState({voiceCommand: userSaid[0]});
+      console.log(this);
+      //setState({voiceCommand: userSaid[0]});
+      // fetch(`https://www.googleapis.com/customsearch/v1?q=${userSaid[0]}&cref=https%3A%2F%2Fcse.google.com%3A443%2Fcse%2Fpublicurl%3Fcx%3D006195244337884894805%3Axugllpj1yoc&cx=006195244337884894805%3Axugllpj1yoc&lr=lang_nl&num=1&key=AIzaSyBaS5tmO3A2z27-fHnJofcMVP94ikmfLUQ`)
+      //   .then(r => r.json())
+      //   .then(d => console.log(d));
+    }.bind(this));
+
+    annyang.start();
+  }
+
+  handleStopVoiceCommands = e => {
+    e.preventDefault();
+    this.setState({voiceActive: false});
+  }
+
   initStream() {
     navigator.getUserMedia (
       {audio: true, video: true},
@@ -82,38 +109,22 @@ class App extends Component {
 
   componentDidMount() {
     this.initStream();
-
-    if (annyang) {
-      const commands = {
-        test: function() {console.log(`Test`);},
-        //'voeg *tag toe': this.setUrl
-        //hello: function() { alert(`Hello world!`); }
-      };
-
-      annyang.addCommands(commands);
-
-      annyang.addCallback(`result`, function(userSaid) {
-        //console.log({userSaid[0]});
-        fetch(`https://www.googleapis.com/customsearch/v1?q=${userSaid[0]}&cref=https%3A%2F%2Fcse.google.com%3A443%2Fcse%2Fpublicurl%3Fcx%3D006195244337884894805%3Axugllpj1yoc&cx=006195244337884894805%3Axugllpj1yoc&lr=lang_nl&num=1&key=AIzaSyBaS5tmO3A2z27-fHnJofcMVP94ikmfLUQ`)
-          .then(r => r.json())
-          .then(d => console.log(d));
-      });
-
-      annyang.addCallback(`end`, function() {console.log(`Sound detection has ended.`);});
-
-      annyang.setLanguage(`nl-NL`);
-      annyang.start();
-    }
-
   }
 
   render() {
-    const {youStream, strangerStream} = this.state;
+    const {youStream, strangerStream, voiceActive, voiceCommand} = this.state;
+    console.log(voiceCommand);
 
     return (
       <main>
           <Video stream={youStream} muted={`true`} />
           <Video stream={strangerStream} muted={`false`} />
+          {voiceActive ? (
+            <button className='google' onClick={this.handleStopVoiceCommands}></button>
+          ) : (
+            <button className='google' onClick={this.handleStartVoiceCommands}></button>
+          )}
+          <div className='result'><p className='result_text'>{voiceCommand}</p></div>
       </main>
     );
   }
